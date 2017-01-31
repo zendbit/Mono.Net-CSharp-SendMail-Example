14d68b072e0f04e922d73e822a24cb7258a0aa07
amru rosyada <amru.rosyada@gmail.com>
2017-01-31 19:24:24 +0700

    Smtp Client Example using Mono.Net C#

---
+++ SmtpClientExample/Program.cs
@@ -0,0 +1,39 @@
+﻿// send mail example using mono .Net C#
+// this example using native smtp module on mono .Net
+using System;
+using System.Net.Mail;
+using System.Text;
+
+namespace SmtpClientExample
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// we try to use outlook email
+			// * Note for gmail you must allow security from unsecured connection
+			// * should enable from google account setting
+
+			string smtp = "your smtp server"; // outlook use smtp-mail.outlook.com
+			string smtpUser = "your smtp username";
+			string smtpPassword = "your smtp password";
+			int port = 587; // default smpt
+
+			// create mail message object
+			var mailMsg = new MailMessage();
+			mailMsg.To.Add("destination mail address");
+			mailMsg.From = new MailAddress("sender mail address");
+			mailMsg.Subject = "Email subject";
+			mailMsg.IsBodyHtml = true; // enable html body, so you can create beaurifull template in html format
+			mailMsg.ReplyToList.Add("add reply address list");
+			mailMsg.Body = "<p>your body email, you can add html format when sending an email</p>";
+			mailMsg.SubjectEncoding = System.Text.Encoding.UTF8;
+			mailMsg.BodyEncoding = System.Text.Encoding.UTF8;
+			mailMsg.HeadersEncoding = System.Text.Encoding.UTF8;
+
+			// let sending the amil
+			var sendMail = new SendMail(smtp, smtpUser, smtpPassword, port);
+			sendMail.Send(mailMsg);
+		}
+	}
+}
---
+++ SmtpClientExample/SendMail.cs
@@ -0,0 +1,44 @@
+﻿// sendmail example using mono .Net C#
+// this example using ssl enable
+
+using System;
+using System.Net.Mail;
+
+namespace SmtpClientExample
+{
+	public class SendMail
+	{
+		string smtp;
+		string username;
+		string password;
+		int port;
+
+
+		public SendMail(string smtp, string username, string password, int port)
+		{
+			this.smtp = smtp;
+			this.username = username;
+			this.password = password;
+			this.port = port;
+		}
+
+		public bool Send(MailMessage message)
+		{
+			try
+			{
+				var client = new System.Net.Mail.SmtpClient(smtp, port);
+				client.EnableSsl = true;
+				client.UseDefaultCredentials = false;
+				client.EnableSsl = true;
+				client.Credentials = new System.Net.NetworkCredential(username, password);
+				client.Send(message);
+				return true;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.StackTrace);
+				return false;
+			}
+		}
+	}
+}
